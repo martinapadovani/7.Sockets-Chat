512a992f9fe3f7946edf6cfeae6c9b6142cbf144
martinapadovani <martinapadovani@gmail.com>
2023-10-23 18:54:36 -0300

    commit

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
-const express = require("express")
-const socket = require("socket.io")
-const path = require("path")
-const fs = require("fs")
+  const express = require("express")
+  const socket = require("socket.io")
+  const path = require("path")
+  const fs = require("fs")
 
-const app = express()
+  //CONFIGURACION DEL SERVIDOR
 
-const PORT = process.env.PORT || 3000
+  //Creo una instancia de Express y le asignas a la variable app
+  const app = express()
 
-app.use(express.static(path.join(__dirname, "public")))
+  const PORT = process.env.PORT || 3000
+  /*Defino el puerto en el que el servidor escuchará, 
+  ya sea el puerto proporcionado por la variable de entorno PORT 
+  o el puerto 3000 de manera predeterminada. */
 
-const server = app.listen(PORT, () => {
-  console.log("El servidor fue levantado en puerto 3000")
-})
+  //CONEXION CON EL FRONT
 
-const io = socket(server)
+  app.use(express.static(path.join(__dirname, "public")))
+  /*Desde el servidor indico la carpeta que contendrá los archivos estaticos (public) con .static
+  .static recibe la ruta de public con .join, que le indico el nombre de la carpeta
+  en la que estoy parado (__dirname) y la carpeta a la que quiero acceder (public)
+   Esto permite que los archivos dentro de "public" sean accesibles desde el navegador
+  */
 
-let jugadores = []
+  //Levanto el servidor con el puerto configurado
+  const server = app.listen(PORT, () => {
+    console.log("El servidor fue levantado en puerto 3000")
+  }) 
 
-io.on("connection", (socket) => {
-  console.log(`Nuevo usuario conectado al socket: ${socket.id}`)
-  io.sockets.emit("player:refresh", jugadores)
+  //CONFIGURACION DE SOCKETS
 
-  // socket.on("chat:mensaje", (mensaje) => {
-  //   io.sockets.emit("chat:mensaje", mensaje)
-  // })
+  //Instancio Socket.io (io) y lo conecto al servidor Express (server)
+  const io = socket(server) //io = (input-output)
+  //Los sockets se configuran y ejecutan sobre el servidor indicado
 
-  // socket.on("chat:escribiendo", (datos) => {
-  //   console.log("Evento recibido")
-  //   socket.broadcast.emit("chat:escribiendo", datos)
-  // })
-  socket.on("player:crear", (jugador) => {
-    jugadores.push(jugador)
-    console.log(jugadores)
-    io.sockets.emit("player:crear", jugador)
+  //Desde io espero el evento connection (propio de la libreria socket.io):
+  io.on("connection", (socket) => {
+    /*cuando se ejecute el evento, se inicia una funcion flecha,
+    que recibe por parametro el socket que se conectó */
+
+    //Definimos eventos propios:
+    
+    //Desde socket escucho/espero el evento chat:mensaje y un mensaje
+    socket.on("chat:mensaje", (mensaje) => {
+      //Desde el servidor recibo el evento con el mensaje, y lo emito a todos los sockets 
+      io.sockets.emit("chat:mensaje", mensaje)
+    })
+
+    socket.on("chat:escribiendo", (username) => {
+      console.log("Evento recibido")
+      //En base al username recibido, desde el socket, transmito la emicion del evento
+      socket.broadcast.emit("chat:escribiendo", username)
+      /*Con broadcast, el evento se transmite a todos los clientes conectados,
+       excepto al cliente que generó el evento */
+    })
+    
   })
-
-  socket.on("player:arriba", (jugador) => {
-    io.sockets.emit("player:arriba", jugador)
-  })
-
-  socket.on("disconnect", (nombre) => {
-    jugadores = jugadores.filter((jugador) => jugador.nombre !== nombre)
-    io.sockets.emit("player:refresh", jugadores)
-  })
-})
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,4 @@
+//Instancia del socket
 const socket = io()
 
 const mensaje = document.getElementById("mensaje")
@@ -6,21 +7,35 @@ const enviar = document.getElementById("enviar")
 const listaDeMensajes = document.getElementById("output")
 const escribiendo = document.getElementById("escribiendo")
 
+//ENVIAR MENSAJE
 enviar.addEventListener("click", () => {
+  /*Cuando el usuario hace clic en el botón "Enviar", el socket va a emitir al servidor
+  el evento "chat:mensaje", junto a los datos del mensaje que se pasan como objeto. */
   socket.emit("chat:mensaje", { usuario: username.value, contenido: mensaje.value })
 
-  mensaje.value = ""
+  mensaje.value = "" //Borro el contenido del campo de entrada de mensajes.
 })
 
-mensaje.addEventListener("keypress", () => {
-  socket.emit("chat:escribiendo", username.value)
-})
-
+//RECIBIR MENSAJE
 socket.on("chat:mensaje", (mensaje) => {
+  /*El socket espera/escucha el evento chat:mensaje que se envía desde el servidor.
+   Cuando se reciba el evento junto al mensaje, lo agrego al html para mostrarlo*/
   listaDeMensajes.innerHTML += `<p> ${mensaje.usuario}: ${mensaje.contenido}</p>`
 })
 
-socket.on("chat:escribiendo", (datos) => {
+//Notificacion de "escribiendo.."
+socket.on("chat:escribiendo", (username) => {
   console.log("Evento recibido")
-  escribiendo.innerHTML = `<p>${datos}</p> esta escribiendo...`
+  /*Cuando el servidor envía el evento "chat:escribiendo", recibo username 
+  y muestro un mensaje "Escribiendo..." en la interfaz*/
+  escribiendo.innerHTML = `<p>${username}</p> esta escribiendo...`
 })
+
+
+//Cuando el usuario presiona una tecla en el input de mensajes
+mensaje.addEventListener("keypress", () => {
+  //Envía un evento "chat:escribiendo" al servidor, junto al username
+  socket.emit("chat:escribiendo", username.value)
+})
+
+
